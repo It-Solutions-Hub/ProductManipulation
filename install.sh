76a77ef8fd95008b726e742060bea6fb413ef422
It-Solutions-Hub <corporateitsolutions@proton.me>
2025-03-27 16:19:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,23 +6,17 @@ sudo yum update -y
 # Install Java 17 (Amazon Corretto)
 sudo yum install java-17-amazon-corretto-devel -y
 
-# Download Apache Tomcat from S3 bucket
-if ! aws s3 cp s3://webapp-bucket-27-03/apache-tomcat-9.0.75-windows-x64.zip apache-tomcat-9.0.75.zip; then
-  echo "ERROR: Failed to download Tomcat from S3" >&2
-  exit 1
-fi
-
-# Extract Tomcat
-sudo unzip apache-tomcat-9.0.75.zip 
-
-# Move Tomcat to /opt directory
-sudo mkdir -p /opt/tomcat
-sudo mv apache-tomcat-9.0.75 /opt/tomcat/
-
-# Grant execution permissions to Tomcat scripts
-sudo chmod +x /opt/tomcat/apache-tomcat-9.0.75/bin/*.sh
-
-# Start Tomcat
-/opt/tomcat/apache-tomcat-9.0.75/bin/startup.sh
+sudo mkdir -p /usr/local/tomcat
+cd /usr/local/tomcat
+# Download Apache Tomcat v8.5:
+sudo curl -O https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.0/bin/apache-tomcat-8.5.0.tar.gz
+# Extract the Tomcat archive:
+sudo tar xvf apache-tomcat-8.5.0.tar.gz
+# Rename the extracted directory to tomcat:
+sudo mv apache-tomcat-8.5.0 tomcat
+# Verify the extracted directory:
+sudo ls -l /usr/local/tomcat/tomcat
+# Start Tomcat:
+sudo /usr/local/tomcat/tomcat/bin/startup.sh
 
 echo "Tomcat 9 installed and started successfully!"
